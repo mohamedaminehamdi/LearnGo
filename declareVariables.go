b3c1aedc739147c2af57188268fd9c5eda636f19
mohamedaminehamdi <mohamedamine.hamdi@insat.ucar.tn>
2025-02-25 22:02:29 +0100

    second commit

---
+++ declareVariables.go
@@ -0,0 +1,12 @@
+package declaraVariables
+
+import("fmt")
+
+func declaraVariables{
+	var s,t string="foo","bar"
+	var (
+		s string = "bonjour"
+		int i=5
+	)
+	s:="hello" 
+}
--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 //first code
-
+/* multi 
+line 
+comment */
 func main(){
-	fmt.Println("Hello World!")
+	title:="Hello World!"
+	var s string = "good bye"
+	var b bool = false
+	var f float64 = 77.89
+	var i int = 5
+	fmt.Printf("%d",i)
+	fmt.Print(s,i,b,f)
+	fmt.Println(title)
 }
